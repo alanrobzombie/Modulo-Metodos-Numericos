f1d76e945eb844c1312efc9f43427f466ef1a619
alanrobzombie <alanplspara3@outlook.com>
2024-03-06 03:04:29 -0600

    Update Bisec.py

--- Met_nums/Root/Bisec.py
+++ Met_nums/Root/Bisec.py
@@ -1,5 +1,5 @@
 import numpy as np
-from tools.moduloBisec import * # importamos nuestros módulos
+from tools.moduloFun import * # importamos nuestros módulos
 
 def bisec(f, interv, error, n_iter=1000, tol=1e-6):
     intervals = search(f, interv)
@@ -12,7 +12,8 @@ def bisec(f, interv, error, n_iter=1000, tol=1e-6):
             b=x_r
         elif(f(a)*f(x_r)>0):
             a=x_r
-        aprox.append(x_r) #se añade a la lista la primera aproximación       
+        aprox.append(x_r) #se añade a la lista la primera aproximación    
+        cont = 0   
         sol=0
         for _ in range(n_iter): # se repetirá el ciclo tantas veces como número de iteraciones el usuario haya especificado.
             x_r=(a+b)/2 # esta variable representa el punto medio entre a y b.
@@ -23,8 +24,9 @@ def bisec(f, interv, error, n_iter=1000, tol=1e-6):
             else:
                 return x_r
             aprox.append(x_r) # se añade a la lista nuestra aproximación obtenida
-            if(error(x_r, aprox[i])<=tol): # se calcula el error entre aprox_i y aprox_(i+1)
+            if(error(x_r, aprox[cont])<=tol): # se calcula el error entre aprox_i y aprox_(i+1)
                 break #se genera un break si se ha llegado al error deseado.
+            cont += 1
             sol = x_r # Actualiza el valor de la aproximación.
         soluciones.append(sol) # Una vez "hallada" la solución, la almancenamos en lista de soluciones.
     return soluciones # retorna la lista de soluciones halladas.
