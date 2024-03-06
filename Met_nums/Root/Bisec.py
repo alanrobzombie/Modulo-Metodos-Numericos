9d7d9ee69b3abbd7dc42f954ce91f0f89d007ec0
alanrobzombie <alanplspara3@outlook.com>
2024-03-06 00:33:17 -0600

    Update Bisec.py

--- Met_nums/Root/Bisec.py
+++ Met_nums/Root/Bisec.py
@@ -1,21 +1,30 @@
 import numpy as np
-from tools.moduloBisec import *
+from tools.moduloBisec import * # importamos nuestros módulos
 
-    
 def bisec(f, interv, error, n_iter=1000, tol=1e-6):
     intervals = search(f, interv)
-    sol=[] # Almacena las soluciones que va encontrando.
+    soluciones=[] # Almacena las soluciones que va encontrando.
+    for i in range(len(interv)): # con esto aseguro que hará la bisección para cada intervalo.
+        a, b = intervals[i, 0], intervals[i,1] # nos da los extremos de cada subintervalo
+        aprox=[] # vamos almacenando las aproximaciones para poder hacer un break al llegar al error deseado.
+        x_r = (a+b)/2 # hacemos bisección una vez para obtener la primera aproximación de la lista.
+        if(f(a)*f(x_r)<0):
+            b=x_r
+        elif(f(a)*f(x_r)>0):
+            a=x_r
+        aprox.append(x_r) #se añade a la lista la primera aproximación       
+        sol=0
         for _ in range(n_iter): # se repetirá el ciclo tantas veces como número de iteraciones el usuario haya especificado.
             x_r=(a+b)/2 # esta variable representa el punto medio entre a y b.
-            if(<0): 
+            if(f(a)*f(x_r)<0): 
                 b=x_r
-            elif(eval(a)*eval(x_r)>0):
-                a=x_r
+            elif(f(a)*f(x_r)>0):
+                a=x_r   
             else:
                 return x_r
-            sol=x_r
-        return sol
-    else:
-        print(check)
-    
-print(bisec(0.5, 1))
+            aprox.append(x_r) # se añade a la lista nuestra aproximación obtenida
+            if(error(x_r, aprox[i])<=tol): # se calcula el error entre aprox_i y aprox_(i+1)
+                break #se genera un break si se ha llegado al error deseado.
+            sol = x_r # Actualiza el valor de la aproximación.
+        soluciones.append(sol) # Una vez "hallada" la solución, la almancenamos en lista de soluciones.
+    return soluciones # retorna la lista de soluciones halladas.
