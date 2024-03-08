0fbd9e4fd1dcd98d1722fc904356ab22294f8750
alanrobzombie <alanplspara3@outlook.com>
2024-03-08 16:17:23 -0600

    Update __init__.py

---
+++ Met_nums/__init__.py
@@ -0,0 +1,5 @@
+from .Root.bisec import bisec
+from .Root.newton import newton
+from .Root.tools.moduloFun import check1, errors, f_signo, search, df
+
+__all__= ['bisec', 'newton', 'check1', 'errors', 'f_signo', 'search', 'df']
